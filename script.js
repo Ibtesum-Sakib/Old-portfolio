ebc2468535197d0e30f6ea5039aaa9b1070b5f06
Mohammad Ibtesum Sakib <30616636+Ibtesum-Sakib@users.noreply.github.com>
2021-01-22 22:22:10 +0600

    updated

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Student", "Developer", "Blogger", "ML Enthusiast", "AI Enthusiast"],
+        strings: ["Student", "Developer", "Blogger", "ML Enthusiast", "Tech Enthusiast","Researcher"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Student", "Developer", "Blogger", "ML Enthusiast", "Tech Enthusiast"],
+        strings: ["Student", "Developer", "Blogger", "ML Enthusiast", "Tech Enthusiast", "Researcher"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
